c1e6d6facb9148a2144f259c69409fa51ec4da04
Abeg3 <167269430+Abeg3@users.noreply.github.com>
2024-04-18 10:55:17 +0800

    Update Arithmetic Operations.py
    With example.

--- Arithmetic Operations.py
+++ Arithmetic Operations.py
@@ -8,6 +8,15 @@ print("subtract=", a - b)
 print("multiply=", a * b)
 print("divide=", a / b)
 
- 
 
+# Once you enter the two operands, the answer for each operation will appear immediately. [EXAMPLE BELOW:]
+
+PS C:\Users\Admin> & C:/Users/Admin/AppData/Local/Microsoft/WindowsApps/python3.11.exe "c:/Users/Admin/OneDrive/Desktop/Arithmetic Operations.py"
+Enter a number:123456789
+Enter a number:987654321
+sum= 1111111110
+subtract= -864197532        
+multiply= 121932631112635269
+divide= 0.1249999988609375  
+PS C:\Users\Admin> 
 
